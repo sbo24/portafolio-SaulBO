7a6185f3bf76dfcfb4d62b6683cc87f02438b6bb
saulbj <alumno.573606@iesazarquiel.es>
2024-08-28 12:29:34 +0200

    somo modification css

--- js/main.js
+++ js/main.js
@@ -5,13 +5,23 @@ $(document).ready(function () {
     // Efecto de scroll suave
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500, 'linear');
+        const target = $(this).attr('href');
+        const $target = $(target);
+
+        // Verifica si el objetivo existe
+        if ($target.length) {
+            $('html, body').animate({
+                scrollTop: $target.offset().top
+            }, 500, 'linear');
+        }
     });
 
     // Cambiar color de la navbar al hacer scroll
     $(window).on('scroll', function () {
-        $navbar.toggleClass('scrolled', $(this).scrollTop() > scrollOffset);
+        const scrollTop = $(this).scrollTop();
+        $navbar.toggleClass('scrolled', scrollTop > scrollOffset);
+
+        // Agregar clase 'scrolling' cuando el usuario se desplaza
+        $navbar.toggleClass('scrolling', scrollTop > 0);
     });
 });
